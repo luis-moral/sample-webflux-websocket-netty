26531226675eaf1a8937a036d64f7d79a2bb2fb8
Luis Moral Guerrero <luis.moral@gmail.com>
2018-02-06 15:25:48 +0100

    Server logic moved to ServerComponent.

--- src/main/java/samples/webflux/websocket/netty/component/ServerComponent.java
+++ src/main/java/samples/webflux/websocket/netty/component/ServerComponent.java
@@ -1,14 +1,52 @@
 package samples.webflux.websocket.netty.component;
 
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import samples.webflux.websocket.netty.handler.MessageDTO;
+import samples.webflux.websocket.netty.handler.WebSocketServerHandler;
+
 @Component
 public class ServerComponent implements ApplicationListener<ApplicationReadyEvent>
 {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private WebSocketServerHandler webSocketServerHandler;
+	
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) 
 	{
+		Flux<MessageDTO> receiveAll =
+			webSocketServerHandler
+				.receive()
+				.subscribeOn(Schedulers.elastic())
+				.doOnNext(message -> logger.info("Received: [{}]", message.getValue()));				
+		
+		Mono<MessageDTO> receiveFirst =
+			webSocketServerHandler				
+				.receive()
+				.subscribeOn(Schedulers.elastic())
+				.next();
+		
+		Flux<MessageDTO> send =
+			Flux
+				.interval(Duration.ofMillis(250))
+				.subscribeOn(Schedulers.elastic())
+				.map(interval -> new MessageDTO(interval))
+				.doOnNext(dto -> logger.info("Sent: [{}]", dto.getValue()))
+				.doOnNext(dto -> webSocketServerHandler.send(dto));
+		
+		receiveAll.subscribe();
+		receiveFirst.thenMany(send).subscribe();
 	}
 }
--- src/main/java/samples/webflux/websocket/netty/handler/WebSocketServerHandler.java
+++ src/main/java/samples/webflux/websocket/netty/handler/WebSocketServerHandler.java
@@ -1,10 +1,9 @@
 package samples.webflux.websocket.netty.handler;
 
-import java.time.Duration;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,22 +18,40 @@ public class WebSocketServerHandler implements WebSocketHandler
 	
 	private final ObjectMapper objectMapper;
 	
+	private final HandlerPublisher<MessageDTO> publisher;
+	private final Flux<MessageDTO> receiveFlux;
+	
+	private WebSocketSession session;
+	
 	public WebSocketServerHandler(ObjectMapper objectMapper)
 	{
 		this.objectMapper = objectMapper;
+		
+		publisher = new HandlerPublisher<MessageDTO>();		
+		receiveFlux = Flux.from(publisher).cache(50);
 	}
 	
 	@Override
 	public Mono<Void> handle(WebSocketSession session) 
 	{
-		Mono<?> receive = 
+		this.session = session;
+		
+		Flux<MessageDTO> receive =		
+			session
+				.receive()
+				.subscribeOn(Schedulers.elastic())
+				.doOnError(t -> logger.error(t.getLocalizedMessage(), t))
+				.map(this::toMessageDTO)
+				.doOnNext(publisher::publish);
+		
+		/*Mono<?> receive = 
 					session
 						.receive()
 						.subscribeOn(Schedulers.elastic())
 						.doOnNext(message -> logger.info("Received: [{}]", message.getPayloadAsText()))
-						.next();
+						.next();*/
 		
-		Mono<Void> send =
+		/*Mono<Void> send =
 					session.send
 					(
 						Flux
@@ -44,8 +61,43 @@ public class WebSocketServerHandler implements WebSocketHandler
 							.map(dto -> { try { return objectMapper.writeValueAsString(dto); } catch (Exception e) { throw new RuntimeException(e); } })
 							.doOnNext(message -> logger.info("Sent: [{}]", message))
 							.map(string -> session.textMessage(string))			
-					);
+					);*/
 		 
-		return receive.then(send).then();
+		return receive.then();
+	}
+	
+	public Flux<MessageDTO> receive()
+	{
+		return receiveFlux;
+	}
+	
+	public void send(MessageDTO message)
+	{
+		session
+			.send(Mono.just(toWebSocketMessage(message)));
+	}
+	
+	private MessageDTO toMessageDTO(WebSocketMessage message)
+	{
+		try 
+		{ 
+			return objectMapper.readValue(message.getPayloadAsText(), MessageDTO.class); 
+		}
+		catch (Exception e) 
+		{ 
+			throw new RuntimeException(e); 
+		}
+	}
+	
+	private WebSocketMessage toWebSocketMessage(MessageDTO message)
+	{
+		try 
+		{ 
+			return session.textMessage(objectMapper.writeValueAsString(message)); 
+		} 
+		catch (Exception e) 
+		{ 
+			throw new RuntimeException(e); 
+		}
 	}
 }
