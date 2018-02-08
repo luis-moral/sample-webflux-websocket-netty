92ba9a270ec6d5dec6608faf53ce2af78179cdd0
Luis Moral Guerrero <luis.moral@gmail.com>
2018-02-08 21:53:40 +0100

    Added list to save sessions.

--- src/main/java/sample/webflux/websocket/netty/handler/ServerWebSocketHandler.java
+++ src/main/java/sample/webflux/websocket/netty/handler/ServerWebSocketHandler.java
@@ -1,5 +1,8 @@
 package sample.webflux.websocket.netty.handler;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
 
@@ -9,22 +12,28 @@ import reactor.core.publisher.ReplayProcessor;
 
 public class ServerWebSocketHandler implements WebSocketHandler
 {
-	private final ReplayProcessor<WebSocketSessionHandler> connectedProcessor;	
+	private final ReplayProcessor<WebSocketSessionHandler> connectedProcessor;
+	private final List<WebSocketSessionHandler> sessionList;
 	
 	public ServerWebSocketHandler()
 	{
 		connectedProcessor = ReplayProcessor.create();
+		sessionList = new LinkedList<WebSocketSessionHandler>();
 	}
 	
 	@Override
 	public Mono<Void> handle(WebSocketSession session) 
-	{		
+	{
 		WebSocketSessionHandler sessionHandler = new WebSocketSessionHandler();
 		
 		sessionHandler
 			.connected()
-			.doOnNext(value -> connectedProcessor.onNext(sessionHandler))			
-			.subscribe();
+			.doOnNext(value -> connectedProcessor.onNext(sessionHandler))
+			.subscribe(value -> sessionList.add(sessionHandler));
+		
+		sessionHandler
+			.disconnected()
+			.subscribe(value -> sessionList.remove(sessionHandler));
 		
 		return sessionHandler.handle(session);
 	}
